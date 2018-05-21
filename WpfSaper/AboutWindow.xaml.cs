5f3f067182b06795c51f4396bde578cbe69d6eee
Jacek Zietek <jacek.zietek@gft.com>
2018-05-21 16:12:06 +0200

    Added initial commands handling and GUI

--- WpfSaper/AboutWindow.xaml.cs
+++ WpfSaper/AboutWindow.xaml.cs
@@ -10,29 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace WpfSaper
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for AboutWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class AboutWindow : Window
     {
-        public MainWindow()
+        public AboutWindow()
         {
             InitializeComponent();
-        }
-
-        private void NewGameCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
-        {
-            e.CanExecute = true;
-        }
-
-        private void NewGameCommand_Executed(object sender, ExecutedRoutedEventArgs e)
-        {
-            MessageBox.Show("The New command was invoked");
         }
     }
 }
---
+++ WpfSaper/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,87 @@
+﻿using Gat.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using WpfSaper.Commands;
+using WpfSaper.Model;
+using WpfSaper.Services;
+using WpfSaper.Services.Impl;
+
+namespace WpfSaper.ViewModel
+{
+    class MainWindowViewModel
+    {
+        public MainWindowViewModel()
+        {
+            this.minefieldFactory = new MinefieldFactory(new RandomBooleansGenerator());
+            this.minefield = this.minefieldFactory.CreateNew(10, 10, 10); //TODO not to be harcoded
+        }
+
+        private ICommand showAboutBoxCommand;
+        private ICommand newGameCommand;
+        private ICommand exitApplicationCommand;
+
+        private Minefield minefield;
+
+        private readonly IMinefieldFactory minefieldFactory;
+
+        public Minefield Minefield
+        {
+            get
+            {
+                return minefield;
+            } 
+        }
+
+        public ICommand ShowAboutBoxCommand
+        {
+            get
+            {
+                if (showAboutBoxCommand == null)
+                {
+                    showAboutBoxCommand = new RelayCommand( (arg) => { ShowAboutBoxWindow(); } );
+                }
+                return showAboutBoxCommand;
+            }
+        }
+
+        public ICommand NewGameCommand
+        {
+            get
+            {
+                if (newGameCommand == null)
+                {
+                    newGameCommand = new RelayCommand((arg) => { StartNewGame(); });
+                }
+                return newGameCommand;
+            }
+        }
+
+        public ICommand ExitApplicationCommand
+        {
+            get
+            {
+                if (exitApplicationCommand == null)
+                {
+                    exitApplicationCommand = new RelayCommand((arg) => { Application.Current.Shutdown(); });
+                }
+                return exitApplicationCommand;
+            }
+        }        
+
+        private void StartNewGame()
+        {
+            MessageBox.Show("TODO Start new game");
+        }
+
+        private void ShowAboutBoxWindow()
+        {
+            AboutWindow aboutWindow = new AboutWindow();
+            aboutWindow.Show();
+        }
+    }
+}
